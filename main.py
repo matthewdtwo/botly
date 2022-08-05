9967df277d146f466b2ee9a7a796a54ddc2fcc2c
Matt Little <matthew@matthewtwo.com>
2022-08-05 15:34:43 -0500

    pushing

--- main.py
+++ main.py
@@ -14,18 +14,26 @@ IN4 = 24
 
 gpio.setup(IN1, gpio.OUT)
 gpio.setup(IN2, gpio.OUT)
+
 gpio.setup(IN3, gpio.OUT)
 gpio.setup(IN4, gpio.OUT)
 
-gpio.output(IN1, gpio.LOW)
-gpio.output(IN2, gpio.HIGH)
-gpio.output(IN3, gpio.LOW)
-gpio.output(IN4, gpio.HIGH)
+
+def forward():
+    gpio.output(IN1, gpio.LOW)
+    gpio.output(IN2, gpio.HIGH)
+    gpio.output(IN3, gpio.LOW)
+    gpio.output(IN4, gpio.HIGH)
 
 
-time.sleep(2)
+def right():
+    gpio.output(IN1, gpio.LOW)
+    gpio.output(IN2, gpio.HIGH)
+    gpio.output(IN3, gpio.HIGH)
+    gpio.output(IN4, gpio.LOW)
 
-gpio.output(IN1, gpio.LOW)
-gpio.output(IN2, gpio.LOW)
-gpio.output(IN3, gpio.LOW)
-gpio.output(IN4, gpio.LOW)
+def stop():
+    gpio.output(IN1, gpio.LOW)
+    gpio.output(IN2, gpio.LOW)
+    gpio.output(IN3, gpio.LOW)
+    gpio.output(IN4, gpio.LOW)

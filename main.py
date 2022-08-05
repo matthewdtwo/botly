a494df08a92f971e2838919d7af0f9c71e9f1ce6
Matt Little <matthew@matthewtwo.com>
2022-08-05 15:37:35 -0500

    pushing

--- main.py
+++ main.py
@@ -12,28 +12,4 @@ IN2 = 27
 IN3 = 23
 IN4 = 24
 
-gpio.setup(IN1, gpio.OUT)
-gpio.setup(IN2, gpio.OUT)
-
-gpio.setup(IN3, gpio.OUT)
-gpio.setup(IN4, gpio.OUT)
-
-
-def forward():
-    gpio.output(IN1, gpio.LOW)
-    gpio.output(IN2, gpio.HIGH)
-    gpio.output(IN3, gpio.LOW)
-    gpio.output(IN4, gpio.HIGH)
-
-
-def right():
-    gpio.output(IN1, gpio.LOW)
-    gpio.output(IN2, gpio.HIGH)
-    gpio.output(IN3, gpio.HIGH)
-    gpio.output(IN4, gpio.LOW)
-
-def stop():
-    gpio.output(IN1, gpio.LOW)
-    gpio.output(IN2, gpio.LOW)
-    gpio.output(IN3, gpio.LOW)
-    gpio.output(IN4, gpio.LOW)
+leftMotor = Motor(IN1, IN2)
--- motor.py
+++ motor.py
@@ -3,25 +3,4 @@ import RPi.GPIO as gpio
 
 class Motor:
     def __init__(self, in1, in2):
-        self.in1 = in1
-        self.in2 = in2
-        gpio.setup(self.in1, gpio.OUT)
-        gpio.setup(self.in2, gpio.OUT)
-        gpio.output(self.in1, gpio.LOW)
-        gpio.output(self.in2, gpio.LOW)
-
-
-    def setDirection(self, direction):
-        if(direction == Direction.FORWARD):
-            print("Forward")
-            gpio.setup(self.in1, gpio.HIGH)
-            gpio.setup(self.in2, gpio.LOW)
-        if(direction == Direction.BACKWARD):
-            print("Backward")
-            gpio.setup(self.in1, gpio.LOW)
-            gpio.setup(self.in2, gpio.HIGH)
-
-        if(direction == Direction.STOPPED):
-            print("Stopped")
-            gpio.setup(self.in1, gpio.LOW)
-            gpio.setup(self.in2, gpio.LOW)
+        print(f"in1: {in1}, in2: {in2}")

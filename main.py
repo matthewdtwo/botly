79c969b668fed0ee71b6a718362d70eeef65dcb3
Matt Little <matthew@matthewtwo.com>
2022-08-05 19:45:29 -0500

    pushing

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ from direction import Direction as Dir
 
 import RPi.GPIO as gpio
 import time
+import flask
+import flask_cors
+
+
+app = Flask(__name__)
 
 gpio.setmode(gpio.BCM)
 gpio.setwarnings(False)
@@ -14,14 +19,28 @@ IN4 = 24
 
 leftMotor = Motor(IN1, IN2)
 rightMotor = Motor(IN3, IN4)
+
+@app.route("/forward")
 def forward():
     leftMotor.setDirection(Dir.FORWARD)
     rightMotor.setDirection(Dir.FORWARD)
 
+@app.route("/stopped")
 def stop():
     leftMotor.setDirection(Dir.STOPPED)
     rightMotor.setDirection(Dir.STOPPED)
 
-forward()
-time.sleep(2)
-stop()
+@app.route("/left")
+def left():
+    leftMotor.setDirection(Dir.FORWARD)
+    rightMotor.setDirection(Dir.BACKWARD)
+
+@app.route("/right")
+def right():
+    leftMotor.setDirection(Dir.BACKWARD)
+    rightMotor.setDirection(Dir.FORWARD)
+
+@app.route("/backward")
+def backward():
+    leftMotor.setDirection(Dir.BACKWARD)
+    rightMotor.setDirection(Dir.BACKWARD)

32e37d0b53c0ab338fcdba7c01737feb0c91bf34
Matt Little <matthew@matthewtwo.com>
2022-08-06 22:24:01 -0500

    testing speed control

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from direction import Direction as Dir
 
 import RPi.GPIO as gpio
 import time
-from flask import Flask
+from flask import Flask, request
 import flask_cors
 
 
@@ -22,6 +22,23 @@ M2_SPEED = 9
 leftMotor = Motor(IN1, IN2, M1_SPEED)
 rightMotor = Motor(IN3, IN4, M2_SPEED)
 
+
+
+@app.route("/axis0", methods=["POST"])
+def axis0():
+    content_type = request.headers.get('Content-Type')
+    if(content_type == 'application/json'):
+        json = request.json
+        print(f"json: {json}")
+    return "axis0"
+
+@app.route("/axis1", methods=["POST"])
+def axis1():
+    content_type = request.headers.get('Content-Type')
+    if(content_type == 'application/json'):
+        json = request.json
+        print(f"json: {json}")
+    return "axis1"
 @app.route("/forward")
 def forward():
     leftMotor.setDirection(Dir.FORWARD)
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Starting the App
+```
+export FLASK_APP=main.py
+flask run -p 5000 -h 0.0.0.0
+```

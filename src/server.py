a7fbae719b1a32b51628620337b0c8374240e652
Matthew Little <matthew@macbook.matthewtwo.com>
2022-09-25 15:12:05 -0500

    testing static content

--- src/server.py
+++ src/server.py
@@ -3,13 +3,12 @@ from direction import Direction as Dir
 
 import RPi.GPIO as gpio
 import time
-from flask import Flask, request
+from flask import Flask, request, send_from_directory
 from flask_cors import CORS
 
 
 app = Flask(__name__)
 CORS(app)
-
 
 gpio.setmode(gpio.BCM)
 gpio.setwarnings(False)
@@ -24,7 +23,9 @@ M2_SPEED = 9
 leftMotor = Motor(IN1, IN2, M1_SPEED)
 rightMotor = Motor(IN3, IN4, M2_SPEED)
 
-
+@app.route("/")
+def send_index():
+    return send_from_directory("static", "index.html")
 
 @app.route("/gamepad", methods=["POST"])
 def axis0():
@@ -33,38 +34,3 @@ def axis0():
         json = request.json
         print(f"json: {json}")
     return "axis0"
-
-
-@app.route("/forward")
-def forward():
-    leftMotor.setDirection(Dir.FORWARD)
-    rightMotor.setDirection(Dir.FORWARD)
-    leftMotor.setSpeed(100)
-    rightMotor.setSpeed(100)
-    return "forward"
-
-@app.route("/stopped")
-def stop():
-    leftMotor.setDirection(Dir.STOPPED)
-    rightMotor.setDirection(Dir.STOPPED)
-    leftMotor.setSpeed(0)
-    rightMotor.setSpeed(0)
-    return "stopped"
-
-@app.route("/left")
-def left():
-    leftMotor.setDirection(Dir.FORWARD)
-    rightMotor.setDirection(Dir.BACKWARD)
-    return "left"
-
-@app.route("/right")
-def right():
-    leftMotor.setDirection(Dir.BACKWARD)
-    rightMotor.setDirection(Dir.FORWARD)
-    return "right"
-
-@app.route("/backward")
-def backward():
-    leftMotor.setDirection(Dir.BACKWARD)
-    rightMotor.setDirection(Dir.BACKWARD)
-    return "backward"

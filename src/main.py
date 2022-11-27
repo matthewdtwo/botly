7d322f608ce89fce661a5de9b4181bb19206fa76
Matthew Little <matthew@matthewtwo.com>
2022-11-27 17:15:43 -0600

    working on more robust serial communication

--- src/main.py
+++ src/main.py
@@ -28,59 +28,16 @@ right_encoder_offset = 0
 left_motor = Motor(IN1, IN2, M1_SPEED)
 right_motor = Motor(IN3, IN4, M2_SPEED)
         
-def set_motor_direction(motors, direction):
-    for motor in motors:
-        motor.setDirection(direction)
-        
-def set_motor_speeds(motors, speed):
-    for motor in motors:
-        motor.setSpeed(speed)
-
-def set_right_offset(offset):
-    global right_encoder_offset
-    if right_encoder_offset == 0:
-        right_encoder_offset = offset
-        
-def set_left_offset(offset):
-    global left_encoder_offset
-    if left_encoder_offset == 0:
-        left_encoder_offset = offset
-
-def set_left_encoder(value):
-    global left_encoder
-    left_encoder = left_encoder_offset - value    
-
-def set_right_encoder(value):
-    global right_encoder
-    right_encoder = right_encoder_offset - value    
-
-def parse_line_to_encoders(line):
-    split_line = line.split()
-    direction = split_line[0]
-    value = int(split_line[1])
-
-    if direction == "right:":
-        if right_encoder_offset == 0:
-            set_right_offset(value)
-        set_right_encoder(value)
-    if direction == "left:":
-        if left_encoder_offset == 0:
-            set_left_offset(value)
-        set_left_encoder(value)  
-
-def try_parse_line(line):
-    if line != "":
-        try:
-            parse_line_to_encoders(line)
-            print(f"left: {left_encoder}, left_encoder_offset {left_encoder_offset}, right: {right_encoder}, right_encoder_offset {right_encoder_offset}")
-            # print(f"line: {line}")
-        except Exception as e:
-            print(f"failed: {e}")
+motors = [left_motor, right_motor]
 
 def read_line():
-    line = ser.readline().decode('utf-8')
-    line = str(line).strip()
-    return line
+    try: 
+        line = ser.readline().decode('utf-8')
+        line = str(line).strip()
+        return line
+    except Exception as read_exception:
+        print(f"read exception: {read_exception}")
+        
 
 def reset_arduino():
     gpio.setup(ARDUINO_RESET, gpio.OUT)
@@ -89,36 +46,17 @@ def reset_arduino():
     gpio.output(ARDUINO_RESET, gpio.HIGH)
     time.sleep(5)
 
-def stop_motors():
-    set_motor_direction(motors, Dir.STOPPED)
-    set_motor_speeds(motors, 0)
-    
-def start_motors():
-    set_motor_direction(motors, Dir.FORWARD)
-    set_motor_speeds(motors, 100)
 # main program variables
 
-# reset_arduino()
-
-ser = serial.Serial("/dev/ttyACM0", 115200, timeout=1)
-
-motors = [left_motor, right_motor]
-
-set_motor_direction(motors, Dir.FORWARD)
-set_motor_speeds(motors, 100)
-
-
-left_encoder_target = 475
-right_encoder_target = 475
-
-# main program loop   
-
-
-
-start_time = int(time.time())
-while ser.is_open:
-    try_parse_line(read_line())
+try :
+    ser = serial.Serial("/dev/ttyACM0", 115200, timeout=1)
+except Exception as e:
+    print(f"Failed opening the serial port: {e}")
+    reset_arduino()
+    try:
+        ser = serial.Serial("/dev/ttyACM0", 115200, timeout=1)
+    except:
+        print("failed twice. exiting")
+        exit(1)
     
-    if (left_encoder >= left_encoder_target) and (right_encoder >= right_encoder_target):
-        stop_motors()
-        
+    

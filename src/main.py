306b6f531c65ff0fc3afe21ccf846c087f6777a7
Matthew Little <matthew@matthewtwo.com>
2022-11-26 20:35:39 -0600

    testing

--- src/main.py
+++ src/main.py
@@ -25,31 +25,6 @@ right_encoder_offset = 0
 
 left_motor = Motor(IN1, IN2, M1_SPEED)
 right_motor = Motor(IN3, IN4, M2_SPEED)
-
-
-def set_left_offset(offset):
-    global left_encoder_offset
-    if left_encoder_offset == 0:
-        left_encoder_offset = offset
-
-def set_right_offset(offset):
-    global right_encoder_offset
-    if right_encoder_offset == 0:
-        right_encoder_offset = offset
-
-        
-def parse_left_or_right_encoder(data_split):
-    global left_encoder_offset
-    global right_encoder_offset
-    global left_encoder
-    global right_encoder
-
-    if data_split[0] == "left:":
-        set_left_offset(data_split[1])
-        left_encoder = int(data_split[1]) - int(left_encoder_offset)
-    elif data_split[0] == "right:":
-        set_right_offset(data_split[1])
-        right_encoder = int(data_split[1]) - int(right_encoder_offset)
         
 def set_motor_direction(motors, direction):
     for motor in motors:
@@ -60,7 +35,8 @@ def set_motor_speeds(motors, speed):
         motor.setSpeed(speed)
 
 
-
+def parse_line(line):
+    print(f"line: {line}")
         
 ser = serial.Serial("/dev/ttyACM0", 115200, timeout=1)
 line = ''
@@ -73,8 +49,7 @@ while ser.is_open:
             continue
         else:
             try:
-                parse_left_or_right_encoder(line)
-                print(f"left: {left}, right: {right}")
+                parse_line(line)
             except:
                 print("failed to parse line")
 
